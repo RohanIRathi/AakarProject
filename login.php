052eb85e076031d8ac68a5a1b24dda0eb053dfb0
Rohan Rathi <rathirohan8@gmail.com>
2020-10-02 16:41:55 +0530

    Adding database file for users

--- login.php
+++ login.php
@@ -41,7 +41,7 @@
                             </div>
                         </div>
                     </div>
-                </div>S
+                </div>
             </div>
         </div>
     </div>
