31e27d3cc834371e0a4d748b029cbf7c197466bf
Rohan Rathi <rathirohan8@gmail.com>
2020-10-17 17:42:24 +0530

    Added base structure for adding employee by HOD

--- admin_4/add_admin.php
+++ admin_4/add_admin.php
@@ -4,7 +4,6 @@
 
 	if($_SESSION['type'] == "admin")
 	{
-		$id = $mysqli -> real_escape_string($_SESSION["id"]);
 		$query = "INSERT INTO `admin`(`username`, `email`, `password`) VALUES (`$username`,`$email`,`$password`)";
 
 		$run = mysqli_query($mysqli, $query);
