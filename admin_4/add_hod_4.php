84a4414bbad58b96d95ded690759342686c7cc91
Rohan Rathi <rathirohan8@gmail.com>
2020-10-19 17:23:56 +0530

    Changed add_hod_4.php

--- admin_4/add_hod_4.php
+++ admin_4/add_hod_4.php
@@ -3,19 +3,7 @@ session_start();
 include('header_4.php'); 
 include('navbar_4.php');
 
-/*
-	include('../php-utils/user_variables.php');
-
-	if($_SESSION['type'] == "admin") {
-		
-		$id = $mysqli -> real_escape_string($_SESSION["id"]);
-		$query = "INSERT INTO `hod`(`username`, `email`, `password`, `admin_id`) VALUES (`$username`,`$email`,`$password`,`$id`)";
-
-		$run = mysqli_query($mysqli, $query);
-	}
-	else
-		echo "Not an admin! Can't add HOD!";
-*/
+include('add_hod.php');
 ?>
 
  <main style="margin-top: 30px;">
