ca2f24369f9738b98cdad7332125412a78a25fb6
Abhay Chaudhari <abhaychaudhari555@gmail.com>
2020-11-03 17:42:24 +0530

    Update new_visitor.php

--- new_visitor.php
+++ new_visitor.php
@@ -112,14 +112,12 @@
                                             </div>
                                         </div>
                                     </div> 
-                                    <div class="form-group">
-                                        <button class="btn btn-custom form-toggle" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-                                        <span class="sr-only"> Toggle navigation</span>Conference Room 
-                                        </button>
                                         
-                                        <button class="btn btn-custom form-toggle" type="button" data-toggle="collapse" data-target="#" aria-expanded="false" aria-controls=""> 
-                                        <span class="sr-only"> Toggle navigation</span> None 
-                                        </button>
+                                        <div class="form-check">
+                                            <label class="form-check-label">
+                                            <input type="checkbox" class="form-check-input" value="" id="checkbox">Conference Room 
+                                        </label>
+                                        </div>
                                         <hr>
                                         <div id="collapseExample" class="navbar-collapse collapse">
                                             <div class="pt-2">
@@ -128,13 +126,13 @@
                                                         <div class="col">
                                                             <div class="form-group pb-2">
                                                                 <label>Room Name</label>
-                                                                <input type="text" class="form-control form-control-user" placeholder="Enter Room Name" required>
+                                                                <input type="text" class="form-control form-control-user" placeholder="Enter Room Name" >
                                                             </div>
                                                         </div>
                                                         <div class="col">
                                                             <div class="form-group pb-2">
                                                                 <label>Purpose of Room</label>
-                                                                <input type="text" class="form-control form-control-user" placeholder="Enter Purpose" required>
+                                                                <input type="text" class="form-control form-control-user" placeholder="Enter Purpose" >
                                                             </div>
                                                         </div>
                                                     </div>
@@ -142,20 +140,20 @@
                                                         <div class="col">
                                                             <div class="form-group pb-2">
                                                                 <label>Start Time</label>
-                                                                <input type="time" class="form-control form-control-user" placeholder="Start Time" name="start_time" required>
+                                                                <input type="time" class="form-control form-control-user" placeholder="Start Time" name="start_time" >
                                                             </div>
                                                         </div>
                                                         <div class="col">
                                                             <div class="form-group pb-2">
                                                                 <label>End Time</label>
-                                                                <input type="time" class="form-control form-control-user" placeholder="End Time" name="end_time"required>
+                                                                <input type="time" class="form-control form-control-user" placeholder="End Time" name="end_time">
                                                             </div>
                                                         </div>
                                                     </div>
                                                 </form>
                                             </div>
                                         </div>
-                                    </div>
+                                   
                                     <button type="submit" name="add_btn" class="btn btn-primary btn-user btn-block" style="border-radius: 10rem;">Add</button>                                
                                     <hr>
                                 </form>
@@ -167,3 +165,63 @@
         </div>
     </div>
 </main>
+<!--
+      
+    <p><input type="checkbox" name="active" id="active" value="1" /> conference book</p>
+    
+    <div id="active_sub">
+        <div id="collapseExample" class="navbar-collapse collapse">
+                                            <div class="pt-2">
+                                            <form method="POST">
+                                                    <div class="row">
+                                                        <div class="col">
+                                                            <div class="form-group pb-2">
+                                                                <label>Room Name</label>
+                                                                <input type="text" class="form-control form-control-user" placeholder="Enter Room Name" >
+                                                            </div>
+                                                        </div>
+                                                        <div class="col">
+                                                            <div class="form-group pb-2">
+                                                                <label>Purpose of Room</label>
+                                                                <input type="text" class="form-control form-control-user" placeholder="Enter Purpose" >
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col">
+                                                            <div class="form-group pb-2">
+                                                                <label>Start Time</label>
+                                                                <input type="time" class="form-control form-control-user" placeholder="Start Time" name="start_time" >
+                                                            </div>
+                                                        </div>
+                                                        <div class="col">
+                                                            <div class="form-group pb-2">
+                                                                <label>End Time</label>
+                                                                <input type="time" class="form-control form-control-user" placeholder="End Time" name="end_time">
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+    </div>
+    
+<script type="text/javascript">
+document.getElementById('active').onclick = function() {
+    // call toggleSub when checkbox clicked
+    // toggleSub args: checkbox clicked on (this), id of element to show/hide
+    toggleSub(this, 'active_sub');
+};
+
+// called onclick of checkbox
+function toggleSub(box, id) {
+    // get reference to related content to display/hide
+    var el = document.getElementById(id);
+    
+    if ( box.checked ) {
+        el.style.display = 'block';
+    } else {
+        el.style.display = 'none';
+    }
+}
+</script>
