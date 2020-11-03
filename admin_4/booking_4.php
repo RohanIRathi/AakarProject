b4a1ace7607aae06fbf732d6edce62688e3c2639
Abhay Chaudhari <abhaychaudhari555@gmail.com>
2020-11-03 17:42:15 +0530

    Update booking_4.php

--- admin_4/booking_4.php
+++ admin_4/booking_4.php
@@ -69,6 +69,15 @@
                         </div>
                     </div>
                 </div>
+                <div class="col-xl-3 col-md-6">
+                    <div class="card bg-danger text-white mb-4">
+                        <div class="card-body">Notification</div>
+                        <div class="card-footer d-flex align-items-center justify-content-between">
+                            <a class="small text-white stretched-link" href="notification.php">View Details</a>
+                            <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </main>
