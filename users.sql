09343e8e77792542276769a8884a72fab4853438
Siddharth Kothari <siddharthkothari655@gmail.com>
2021-07-14 15:33:23 +0530

    database update

--- users.sql
+++ users.sql
@@ -5,7 +5,7 @@ USE `aakar`;
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 08, 2021 at 06:01 AM
+-- Generation Time: Jul 14, 2021 at 12:02 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.5
 
@@ -35,15 +35,17 @@ CREATE TABLE `admin` (
   `email` varchar(45) NOT NULL,
   `password` text DEFAULT NULL,
   `first_name` varchar(45) NOT NULL,
-  `last_name` varchar(45) NOT NULL
+  `last_name` varchar(45) NOT NULL,
+  `phone_no` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`id`, `username`, `email`, `password`, `first_name`, `last_name`) VALUES
-('00001', '', 'admin@gmail.com', '$2y$10$rSDD/KcGB9Yd6KRJNhSCmef9fbnaJ0B9nlmThEeNtsdF77bG8QoM2', 'Admin', '');
+INSERT INTO `admin` (`id`, `username`, `email`, `password`, `first_name`, `last_name`, `phone_no`) VALUES
+('00001', '', 'admin@gmail.com', '$2y$10$rSDD/KcGB9Yd6KRJNhSCmef9fbnaJ0B9nlmThEeNtsdF77bG8QoM2', 'Admin', '', ''),
+('00002', '', 'admin2@gmail.com', '$2y$10$qTDaJtBXocZO4uOP3wZdP.88XWkrn5wHGQaUUx8APdBvOvWprV1.O', 'admin2', '', '7021019903');
 
 -- --------------------------------------------------------
 
@@ -59,15 +61,16 @@ CREATE TABLE `employee` (
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
   `hod_id` varchar(20) NOT NULL,
-  `hod_name` varchar(20) NOT NULL
+  `hod_name` varchar(20) NOT NULL,
+  `phone_no` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `employee`
 --
 
-INSERT INTO `employee` (`employee_id`, `username`, `email`, `password`, `first_name`, `last_name`, `hod_id`, `hod_name`) VALUES
-('30001', '', 'employee1@gmail.com', '$2y$10$yAEkOUZ.Kmc1mf65ksb6t.Q5g7jJEwlWvbNm35OQ92unHBkhYhEjO', 'EMP', '1', '20001', '');
+INSERT INTO `employee` (`employee_id`, `username`, `email`, `password`, `first_name`, `last_name`, `hod_id`, `hod_name`, `phone_no`) VALUES
+('30001', '', 'employee1@gmail.com', '$2y$10$yAEkOUZ.Kmc1mf65ksb6t.Q5g7jJEwlWvbNm35OQ92unHBkhYhEjO', 'EMP', '1', '20001', '', '');
 
 -- --------------------------------------------------------
 
@@ -96,8 +99,7 @@ CREATE TABLE `emp_leave_pass` (
 --
 
 INSERT INTO `emp_leave_pass` (`leave_pass_id`, `employee_id`, `emp_name`, `hod_id`, `Purpose`, `start_time`, `end_time`, `timestamp`, `status`, `actual_start_time`, `actual_end_time`, `date_of_leave`, `reason`) VALUES
-(52, '30001', 'EMP 1', '20001', 'Reason', '12:17', '14:18', '1625712562', 'ACCEPTED_FIN', '1625713828', '1625713843', '08/07/2021', 'profession'),
-(53, '30001', 'EMP 2', '20001', 'Reason', '12:38', '14:38', '1625713695', 'ONGOING', '1625714619', '', '08/07/2021', 'personal');
+(54, '30001', 'EMP 1', '20001', 'I have to go and meet my friend urgently', '15:48', '18:48', '1625732336', 'ACCEPTED_FIN', '1625733371', '1625733616', '08/07/2021', 'Personal');
 
 -- --------------------------------------------------------
 
@@ -112,16 +114,18 @@ CREATE TABLE `hod` (
   `password` text NOT NULL,
   `first_name` varchar(45) NOT NULL,
   `last_name` varchar(45) NOT NULL,
-  `admin_id` varchar(20) NOT NULL
+  `admin_id` varchar(20) NOT NULL,
+  `phone_no` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `hod`
 --
 
-INSERT INTO `hod` (`hod_id`, `username`, `email`, `password`, `first_name`, `last_name`, `admin_id`) VALUES
-('20001', '', 'hod@gmail.com', '$2y$10$no4wP.ZB0GrdO/YYD7lzqOiD9ZiSGV9qtyJEipLmo2eBLa8t9DLQW', 'HOD', '1', '00001'),
-('20002', '', 'hod2@gmail.com', '$2y$10$M4z5eXjba4qMkVDo0rvktOGUZdOaAgsTUxZEXu3nmw54Gp5glkyba', 'HOD', '2', '00001');
+INSERT INTO `hod` (`hod_id`, `username`, `email`, `password`, `first_name`, `last_name`, `admin_id`, `phone_no`) VALUES
+('20001', '', 'hod@gmail.com', '$2y$10$no4wP.ZB0GrdO/YYD7lzqOiD9ZiSGV9qtyJEipLmo2eBLa8t9DLQW', 'HOD', '1', '00001', ''),
+('20002', '', 'hod2@gmail.com', '$2y$10$M4z5eXjba4qMkVDo0rvktOGUZdOaAgsTUxZEXu3nmw54Gp5glkyba', 'HOD', '2', '00001', ''),
+('20003', '', 'hod3@gmail.com', '$2y$10$XD.VVeXzFn0EY/ImqS5An.IgSi6ykkrhBmAG/SFe91CNMIOWgZEjW', 'hod3', '', '00001', '7021019903');
 
 -- --------------------------------------------------------
 
@@ -135,15 +139,16 @@ CREATE TABLE `security` (
   `email` varchar(45) NOT NULL,
   `password` text NOT NULL,
   `first_name` varchar(45) NOT NULL,
-  `last_name` varchar(45) NOT NULL
+  `last_name` varchar(45) NOT NULL,
+  `phone_no` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `security`
 --
 
-INSERT INTO `security` (`id`, `username`, `email`, `password`, `first_name`, `last_name`) VALUES
-('10001', '', 'admin_security@gmail.com', '$2y$10$z5qh8BHQb1zxnEeyfBVayOXYcJaWWPTGABv4nxI.BmFL0nTlFjsby', 'Admin', '');
+INSERT INTO `security` (`id`, `username`, `email`, `password`, `first_name`, `last_name`, `phone_no`) VALUES
+('10001', '', 'admin_security@gmail.com', '$2y$10$z5qh8BHQb1zxnEeyfBVayOXYcJaWWPTGABv4nxI.BmFL0nTlFjsby', 'Admin', '', '');
 
 -- --------------------------------------------------------
 
@@ -173,6 +178,13 @@ CREATE TABLE `visitor` (
   `dateofappointment` varchar(10) NOT NULL,
   `status` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `visitor`
+--
+
+INSERT INTO `visitor` (`id`, `first_name`, `last_name`, `email`, `purpose`, `photo_id`, `time`, `noofvisitors`, `photo_id_no`, `hospitality`, `conference`, `conference_room`, `room_purpose`, `start_time`, `end_time`, `visitee`, `tokenid`, `phone_no`, `dateofappointment`, `status`) VALUES
+(60, 'Pratyush', 'Kothari', 'admin@gmail.com', 'on', '', '1625764260', 1, 12, 0, 0, '', '', '1625742790', '1625742864', '00001', '$2y$10$nMf3XYwEh4HnORxJyHi8G.HSrnB9fXLnisDo3l9d.nAVv3zEsUXcu', '12', '08-07-21', 'ACCEPTED_FINISHED');
 
 --
 -- Indexes for dumped tables
@@ -226,13 +238,13 @@ ALTER TABLE `visitor`
 -- AUTO_INCREMENT for table `emp_leave_pass`
 --
 ALTER TABLE `emp_leave_pass`
-  MODIFY `leave_pass_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `leave_pass_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT for table `visitor`
 --
 ALTER TABLE `visitor`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- Constraints for dumped tables
